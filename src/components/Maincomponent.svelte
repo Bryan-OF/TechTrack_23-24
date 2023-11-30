0efcbb9124d995d56f0b480ec1d76bfa5296c9e6
Bryan Ortiz Franco <brian.ortizfranco@hotmail.com>
2023-11-30 01:05:01 +0100

    Documentatie toegevoegd
    Ik heb overal commentaar toegevoegd waardoor de code beter te begrijpen is voor andere en voor mezelf
    
    daarnaast heb ik wat styling gegeven aan de code

--- src/components/Maincomponent.svelte
+++ src/components/Maincomponent.svelte
@@ -2,20 +2,23 @@
 	import { onMount } from 'svelte';
 	import * as d3 from 'd3';
 
+	// Geëxporteerde variabelen voor gebruik buiten het component
 	export let activeGame;
 	export let metaScore = null;
-    export let graphData = [];
-    export let showLess = true;
-    export let graphDataLess = [];
-    export let graphDataMore = [];
+    export let graphData = []; // Data voor de grafiek
+    export let showLess = true; // Status om meer of minder data te tonen
+    export let graphDataLess = []; // Data voor de optie "minder tonen"
+    export let graphDataMore = []; // Data voor de optie "meer tonen"
 
+	// Functie om API data op te halen
 	async function fetchApiData(url) {
 		const response = await fetch(url);
 		return await response.json();
 	}
 
+	// Functie om een staafdiagram te maken
     function makeBarChart() {
-        		// Afmetingen en marges van de grafiek
+        // Instellen van afmetingen en marges voor de grafiek
 		const margin = { top: 20, right: 30, bottom: 40, left: 90 },
 			width = 460 - margin.left - margin.right,
 			height = 400 - margin.top - margin.bottom;
@@ -29,14 +32,14 @@
 			.append('g')
 			.attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-		// X axis (Horizontale As)
+		// X-as (Horizontale as)
 		const x = d3
 			.scaleLinear()
-			.domain([0, 100]) // Max prijs is 100 / moet nog aanpassen
+			.domain([0, 60])
 			.range([0, width]);
 		svg.append('g').attr('transform', `translate(0, ${height})`).call(d3.axisBottom(x));
 
-		// Y axis (Verticale as)
+		// Y-as (Verticale as)
 		const y = d3
 			.scaleBand()
 			.range([0, height])
@@ -62,62 +65,70 @@
 			.enter()
 			.append('text')
 			.attr('class', 'label')
-			.attr('x', (d) => x(d.price) + 5) // ruimte van de prijs af
+			.attr('x', (d) => x(d.price) + 5) // Ruimte vanaf de prijs
 			.attr('y', (d) => y(d.storeName) + y.bandwidth() / 2 + 5)
-			.text((d) => `€${d.price.toFixed(2)}`) // De Prijs
-			.attr('text-anchor', 'start') // Tekst x-positie
+			.text((d) => `€${d.price.toFixed(2)}`) // De prijs
+			.attr('text-anchor', 'start') // Positie van de tekst op de x-as
 			.style('fill', 'black') // Kleur van de tekst
 			.style('font-size', '12px'); // Grootte van de tekst
     };
 
+	// Zorgt dat de functie wordt uitgevoerd wanneer het component geladen is
 	onMount(async () => {
 		console.log(activeGame);
-		// API URLs
+		// API URL's
 		const dealsUrl = `https://www.cheapshark.com/api/1.0/deals?title=${activeGame.internalName}&exact`;
 		const storesUrl = 'https://www.cheapshark.com/api/1.0/stores';
 
-		// Fetch API data
+		// Ophalen van API data
 		const dealsData = await fetchApiData(dealsUrl);
 		const storesData = await fetchApiData(storesUrl);
 
 		// Itereren door elk 'deal' object in de dealsData array
 		for (let deal of dealsData) {
-			// Zoekt naar de winkel in storesData die overeenkomt met de storeID van de huidige deal
+			// Zoeken naar de winkel in storesData die overeenkomt met de storeID van de huidige deal
 			const store = storesData.find((s) => s.storeID === deal.storeID);
-			// Controleert of de overeenkomende winkel is gevonden
+			// Controleren of de overeenkomende winkel gevonden is
 			if (store) {
 				graphData.push({
 					storeName: store.storeName,
 					price: parseFloat(deal.salePrice),
 					score: deal.metacriticScore
-				}); // Voeg een object met winkelnaam en dealprijs aan de grafiek
-				// parseFLoat is een string omzet in een floating-point getal (een getal met decimalen) om de price in een nieuw opject te zetten
+				}); // Voegt een object met winkelnaam en dealprijs toe aan de grafiek
+				// parseFloat zet een string om in een floating-point getal (een getal met decimalen) om de prijs in een nieuw object te zetten
 			}
 		}
 
+		// Maakt een lijst van unieke winkels uit 'graphData'.
 		graphData = Object.values(
+			// Voegt nieuwe winkel toe als deze nog niet in de lijst staat.
 			graphData.reduce((unique, o) => {
 				if (!unique[o.storeName]) unique[o.storeName] = o;
 
+				// Geeft de bijgewerkte lijst terug.
 				return unique;
 			}, {})
 		);
 
+		// Kiest de eerste 5 winkels voor een kortere lijst.
 		graphDataLess = graphData.slice(0, 5);
+		// Kiest de eerste 20 winkels voor een langere lijst.
         graphDataMore = graphData.slice(0, 20);
+		// Gebruikt de kortere lijst voor de grafiek.
         graphData = graphDataLess;
 
-            makeBarChart();
+		// Maakt de grafiek met de geselecteerde winkels.
+        makeBarChart();
 
-		// set the dimensions and margins of the graph
+		// Instellen van de afmetingen en marges van de grafiek
 		const widthDonot = 450,
 			heightDonot = 450,
 			marginDonot = 40;
 
-		// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+		// De radius van de taartgrafiek is de helft van de breedte of hoogte (de kleinste van de twee). Ik trek er een beetje marge van af.
 		const radius = Math.min(widthDonot, heightDonot) / 2 - marginDonot;
 
-		// append the svg object to the div called 'my_dataviz'
+		// Voeg het SVG object toe aan de div met de naam 'my_dataviz'
 		const svgDonot = d3
 			.select('#my_dataviz')
 			.append('svg')
@@ -126,19 +137,19 @@
 			.append('g')
 			.attr('transform', `translate(${widthDonot / 2},${heightDonot / 2})`);
 
-		// Create dummy data
+		// Maak dummy data
 		const data = { a: 100 - Number(graphData[0].score), b: 100 };
 		metaScore = graphData[0].score;
 
-		// set the color scale
+		// Instellen van de kleurenschaal
 		const color = d3.scaleOrdinal().range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56']);
 
-		// Compute the position of each group on the pie:
+		// Bereken de positie van elke groep in de taartgrafiek:
 		const pie = d3.pie().value((d) => d[1]);
 
 		const data_ready = pie(Object.entries(data));
 
-		// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+		// Maak de taartgrafiek: Elke deel van de taart is een pad dat we bouwen met behulp van de arc-functie.
 		svgDonot
 			.selectAll('whatever')
 			.data(data_ready)
@@ -147,7 +158,7 @@
 				'd',
 				d3
 					.arc()
-					.innerRadius(100) // This is the size of the donut hole
+					.innerRadius(100) // Dit is de grootte van het gat in de donut
 					.outerRadius(radius)
 			)
 			.attr('fill', (d) => color(d.data[0]))
@@ -156,23 +167,31 @@
 			.style('opacity', 0.7);
 	});
 
+    // Wisselt tussen meer of minder gegevens in de grafiek.
     function onClickShowMore() {
+        // Soort ervoor dat de grafiek wisselt tussen de korte en lange versie van de grafiekdata.
 		if (showLess) {
-            showLess = false;
-            graphData = graphDataMore;
+            showLess = false; // Schakelt naar de lange lijst.
+            graphData = graphDataMore; // Gebruikt de lange lijst voor de grafiek.
         } else {
-            showLess = true;
-            graphData = graphDataLess;
+            showLess = true; // Schakelt terug naar de korte lijst.
+            graphData = graphDataLess; // Gebruikt de korte lijst voor de grafiek.
         }
+        // Verwijdert de oude grafiek en bouwt een nieuwe grafiek.
         document.getElementById('bar-chart').innerHTML = '';
+        // Maakt de grafiek opnieuw met de nieuwe data.
         makeBarChart();
+        // Toont de huidige status van de data in de console voor debuggen.
         console.log(graphDataMore, graphDataLess, graphData);
 	}
+
 </script>
 
+<!-- HTML elementen voor de grafiek en de knop -->
 <div id="bar-chart" />
-<button on:click={() => onClickShowMore()}>show {showLess ? 'more' : 'less'}</button>
+<button on:click={() => onClickShowMore()}>Toon {showLess ? 'meer' : 'minder'}</button>
 
+<!-- Toon de Metascore grafiek als een game deze heeft -->
 {#if metaScore}
 	<h3>Metascore: {metaScore}</h3>
 {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,41 +6,50 @@
 	let searchedGameResults = [];
 	let activeGame;
 
+    // Functie om games te zoeken op basis van de zoekinput.
 	async function searchGame() {
-		const response = await fetch(searchApi + searchInput);
-		const games = await response.json();
-		searchedGameResults = games.slice(0, 10);
+		const response = await fetch(searchApi + searchInput); // Haalt gegevens op van de API dat samenkomt met zoekinput.
+		const games = await response.json(); // Zet de respons om naar JSON.
+		searchedGameResults = games.slice(0, 10); // Slaat de eerste 10 resultaten op.
 	}
 
+    // Voort deze functies uit wanneer op een spel wordt geklikt.
 	function onClickGame(game) {
-		activeGame = null;
-		setTimeout(() => {
-			activeGame = game;
-			searchInput = '';
-			searchGame();
+		activeGame = null; // Reset de actieve game.
+		setTimeout(() => { // Stelt uitvoering kort uit voor stabiliteit.
+			activeGame = game; // Update actieve game.
+			searchInput = ''; // Maakt Searchbar leeg.
+			searchGame(); // Voert een nieuwe zoekopdracht uit.
 		}, 10);
 	}
 </script>
 
 <main>
-	<h1>Game Checker</h1>
-	<input
-		type="text"
-		id="myInput"
-		bind:value={searchInput}
-		on:input={searchGame}
-		placeholder="Search for a Game"
-	/>
+	<div class="search-container">
+		<h1>Game Checker</h1>
+		<!-- Zoekveld voor het invoeren van de game titel. -->
+		<input
+			type="text"
+			id="myInput"
+			bind:value={searchInput}
+			on:input={searchGame}
+			placeholder="Search for a Game"
+		/>
+	</div>
 
-	<ul>
-		{#each searchedGameResults as game}
-			<button on:click={() => onClickGame(game)} class="game-item">
-				<li>{game.external}</li>
-				<img class="game-thumb" src={game.thumb} alt={game.external} />
-			</button>
-		{/each}
-	</ul>
+	<div class="results-container">
+		<ul>
+			<!-- Toont zoekresultaten als knoppen. -->
+			{#each searchedGameResults as game}
+				<button on:click={() => onClickGame(game)} class="game-item">
+					<img class="game-thumb" src={game.thumb} alt={game.external} />
+					<li>{game.external}</li>
+				</button>
+			{/each}
+		</ul>
+	</div>
 	{#if activeGame}
+		<!-- Toont details van het actieve spel. -->
 		<h3>{activeGame.external}</h3>
 		<img src={activeGame.thumb} alt={activeGame.external} />
 		<Maincomponent bind:activeGame />
@@ -53,6 +62,13 @@
 		max-height: 80px;
 	}
 
+	.search-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
 	#myInput {
 		width: 700.8px;
 		padding: 19.2px;
@@ -62,6 +78,11 @@
 		display: inline-flex;
 		align-items: center;
 		gap: 9.6px;
+	}
+
+	.results-container {
+		height: 400px;
+		overflow: auto;
 	}
 
 	main {
@@ -88,23 +109,23 @@
 	}
 
 	ul li button {
-		border: 1px solid #ddd; /* Add a border to all links */
-		margin-top: -1px; /* Prevent double borders */
-		background-color: #f6f6f6; /* Grey background color */
-		padding: 12px; /* Add some padding */
-		text-decoration: none; /* Remove default text underline */
-		font-size: 18px; /* Increase the font-size */
-		color: black; /* Add a black text color */
-		display: block; /* Make it into a block element to fill the whole list */
+		border: 1px solid #ddd;
+		margin-top: -1px;
+		background-color: #f6f6f6;
+		padding: 12px;
+		text-decoration: none;
+		font-size: 18px;
+		color: black;
+		display: block;
 	}
 
 	ul li button:hover:not(.header) {
-		background-color: #eee; /* Add a hover effect to all links, except for headers */
+		background-color: #eee;
 	}
 
 	.game-item {
-		display: flex; /* Voeg flex display toe */
-		align-items: center; /* Centreer de items verticaal */
-		gap: 10px; /* Voeg wat ruimte toe tussen de naam en de thumbnail */
+		display: flex;
+		align-items: center;
+		gap: 10px;
 	}
 </style>
